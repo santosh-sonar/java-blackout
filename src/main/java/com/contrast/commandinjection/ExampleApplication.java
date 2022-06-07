747d0393ebb2e2b50d85c35cc1123d1f863c6b84
Jonathan Harper <39912347+jharper-sec@users.noreply.github.com>
2022-06-07 15:12:55 -0400

    Added getRuntime().exec vuln

--- src/main/java/com/contrast/commandinjection/ExampleApplication.java
+++ src/main/java/com/contrast/commandinjection/ExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.FileNotFoundException;
@@ -19,12 +20,12 @@ public class ExampleApplication {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
-	@GetMapping("/read")
-	public String get(@RequestParam String command) throws IOException {
+	@GetMapping("/processBuilder")
+	public String processBuilder(@RequestParam String command) throws IOException {
 		try {
 			ProcessBuilder pb = new ProcessBuilder(command);
 			Process process = pb.start();
-			
+
 			String result = new String(process.getInputStream().readAllBytes());
 			return String.format("%s", result);
 		} catch (Exception ex) {
@@ -32,34 +33,50 @@ public class ExampleApplication {
 		}
 	}
 
+	@GetMapping("/exec")
+	public String exec(@RequestParam String command) throws IOException {
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+			String result = new String(process.getInputStream().readAllBytes());
+			return String.format("%s", result);
+		} catch (Exception ex) {
+			return ex.getMessage();
+		}
+	}
+
+	// @GetMapping("/el")
+	// public String el(@RequestParam String message) {
+	// 	return "redirect:/error?msg=error.generic";
+	// }
+
 	// @PostMapping("/create")
 	// public String create(@RequestParam String filename) throws IOException {
-	// 	try {
-	// 		Process process = Runtime.getRuntime().exec("touch " + filename);
-	// 		return String.format("PID: %s", process.pid());
-	// 	} catch (Exception ex) {
-	// 		throw new FileNotFoundException();
-	// 	}
+	// try {
+	// Process process = Runtime.getRuntime().exec("touch " + filename);
+	// return String.format("PID: %s", process.pid());
+	// } catch (Exception ex) {
+	// throw new FileNotFoundException();
+	// }
 	// }
 
 	// @PutMapping("/update")
 	// public String update(@RequestParam String filename) throws IOException {
-	// 	try {
-	// 		Process process = Runtime.getRuntime().exec("touch " + filename);
-	// 		return String.format("PID: %s", process.pid());
-	// 	} catch (Exception ex) {
-	// 		throw new FileNotFoundException();
-	// 	}
+	// try {
+	// Process process = Runtime.getRuntime().exec("touch " + filename);
+	// return String.format("PID: %s", process.pid());
+	// } catch (Exception ex) {
+	// throw new FileNotFoundException();
+	// }
 	// }
 
 	// @DeleteMapping
 	// public String delete(@RequestParam String filename) throws IOException {
-	// 	try {
-	// 		Process process = Runtime.getRuntime().exec("touch " + filename);
-	// 		return String.format("PID: %s", process.pid());
-	// 	} catch (Exception ex) {
-	// 		throw new FileNotFoundException();
-	// 	}
-	// }	
+	// try {
+	// Process process = Runtime.getRuntime().exec("touch " + filename);
+	// return String.format("PID: %s", process.pid());
+	// } catch (Exception ex) {
+	// throw new FileNotFoundException();
+	// }
+	// }
 
 }
